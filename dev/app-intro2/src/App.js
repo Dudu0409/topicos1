d72c1e75fbfd6f0164f0b53d0ce5a95cc3a8bd35
Eduardo Bonamigo Bianchetti <185961@upf.br>
2022-05-03 22:09:35 -0300

    Modularização de Colaborador e melhorias gerais

--- dev/app-intro2/src/App.js
+++ dev/app-intro2/src/App.js
@@ -1,158 +1,59 @@
 import "./App.css";
-
-import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ColaboradorList from "./ColaboradorList";
-import ColaboradorForm from "./ColaboradorForm";
-import ColaboradorSrv from "./services/ColaboradorSrv";
-import PrimeReact from "primereact/api";
+import React, { Suspense, lazy } from "react";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
-import { Toast } from "primereact/toast";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { Menubar } from "primereact/menubar";
+
+const Home = lazy(() => import("./pages/home/Home"));
+const ColaboradorCon = lazy(() => import("./pages/colaborador/ColaboradorCon"));
 
 function App() {
-  const [colaboradores, setColaboradores] = useState([]);
-  const toastRef = useRef();
-  useEffect(() => {
-    onClickAtualizar(); // ao inicializar execula método para atualizar
-  }, []);
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/colaboradores" element={<ColaboradorCon />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
 
-  const onClickAtualizar = () => {
-    ColaboradorSrv.listar()
-      .then((response) => {
-        setColaboradores(response.data);
-        toastRef.current.show({
-          severity: "success",
-          summary: "Colaboradores atualizados",
-          life: 3000,
-        });
-      })
-      .catch((e) => {
-        toastRef.current.show({
-          severity: "error",
-          summary: e.message,
-          life: 3000,
-        });
-      });
-  };
-  const excluir = (_id) => {
-    confirmDialog({
-      message: "Confirma a exclusão?",
-      header: "Confirmação",
-      icon: "pi pi-question",
-      acceptLabel: "Sim",
-      rejectLabel: "Não",
-      acceptClassName: "p-button-danger",
-      accept: () => excluirConfirm(_id),
-    });
-  };
-  const excluirConfirm = (_id) => {
-    ColaboradorSrv.excluir(_id)
-      .then((response) => {
-        onClickAtualizar();
-        toastRef.current.show({
-          severity: "success",
-          summary: "Excluído",
-          life: 2000,
-        });
-      })
-      .catch((e) => {
-        toastRef.current.show({
-          severity: "error",
-          summary: e.message,
-          life: 4000,
-        });
-      });
-  };
-  const initialState = { id: null, nome: "", email: "", senha: "" };
-  const [colaborador, setColaborador] = useState(initialState);
-  const [editando, setEditando] = useState(false);
-  const inserir = () => {
-    setColaborador(initialState);
-    setEditando(true);
-  };
-  const salvar = () => {
-    if (colaborador._id == null) {
-      // inclussão
-      ColaboradorSrv.incluir(colaborador)
-        .then((response) => {
-          setEditando(false);
-          onClickAtualizar();
-          toastRef.current.show({
-            severity: "success",
-            summary: "Salvou",
-            life: 2000,
-          });
-        })
-        .catch((e) => {
-          toastRef.current.show({
-            severity: "error",
-            summary: e.message,
-            life: 4000,
-          });
-        });
-    } else {
-      // alteração
-      ColaboradorSrv.alterar(colaborador)
-        .then((response) => {
-          setEditando(false);
-          onClickAtualizar();
-          toastRef.current.show({
-            severity: "success",
-            summary: "Salvou",
-            life: 2000,
-          });
-        })
-        .catch((e) => {
-          toastRef.current.show({
-            severity: "error",
-            summary: e.message,
-            life: 4000,
-          });
-        });
-    }
-  };
-  const cancelar = () => {
-    console.log("Cancelou ...");
-    setEditando(false);
-  };
+function Menu() {
+  let navigate = useNavigate();
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-fw pi-home",
+      command: () => {
+        navigate("/");
+      },
+    },
+    {
+      label: "Cadastro",
+      icon: "pi pi-fw pi-file",
+      items: [
+        {
+          label: "Colaboradores",
+          icon: "pi pi-fw pi-user",
+          command: () => {
+            navigate("/colaboradores");
+          },
+        },
+      ],
+    },
+    {
+      label: "Sair",
+      icon: "pi pi-fw pi-power-off",
+    },
+  ];
 
-  const editar = (id) => {
-    setColaborador(
-      colaboradores.filter((colaborador) => colaborador._id == id)[0]
-    );
-    setEditando(true);
-  };
-
-  if (!editando) {
-    return (
-      <div className="App">
-        <ConfirmDialog />
-        <ColaboradorList
-          colaborador={colaboradores}
-          onClickAtualizar={onClickAtualizar}
-          inserir={inserir}
-          editar={editar}
-          excluir={excluir}
-        />
-        <Toast ref={toastRef} />
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <ColaboradorForm
-          colaborador={colaborador}
-          setColaborador={setColaborador}
-          salvar={salvar}
-          cancelar={cancelar}
-        />
-        <Toast ref={toastRef} />
-      </div>
-    );
-  }
+  return <Menubar model={items} />;
 }
 
 export default App;
---
+++ dev/app-intro2/src/pages/colaborador/ColaboradorForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+const ColaboradorForm = (props) => {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    props.setColaborador({ ...props.colaborador, [name]: value });
+  };
+
+  return (
+    <form>
+      <div className="form-group">
+        <label>Nome</label>
+        <input
+          className="form-control"
+          type="text"
+          name="nome"
+          value={props.colaborador.nome}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          className="form-control"
+          type="text"
+          name="email"
+          value={props.colaborador.email}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <label>Senha</label>
+        <input
+          className="form-control"
+          type="password"
+          name="senha"
+          value={props.colaborador.senha}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <button
+          type="button"
+          onClick={props.salvar}
+          className="btn btn-primary btn-sm"
+        >
+          Salvar
+        </button>
+        <button
+          type="button"
+          onClick={props.cancelar}
+          className="btn btn-primary btn-sm"
+        >
+          Cancelar
+        </button>
+      </div>
+    </form>
+  );
+};
+export default ColaboradorForm;
